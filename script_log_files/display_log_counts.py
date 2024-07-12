3efdc538258438913b7cd1fe5c825e4a435964b3
Deminform <deminform@gmail.com>
2024-07-12 12:34:45 +0200

    All methods are accomplished

--- script_log_files/display_log_counts.py
+++ script_log_files/display_log_counts.py
@@ -1,10 +1,28 @@
 from prettytable import PrettyTable
+from colorama import Fore, Style
 
 
 def display_log_counts(counts: dict):
     table = PrettyTable()
+    table.align = 'l'
     table.field_names = ['Рівень логування', 'Кількість']
 
     for key, value in counts.items():
-        table.add_row([key, value])
+        table.add_row([coloring(key), value])
     return table
+
+
+def coloring(text: str):
+    match text:
+        case 'INFO':
+            return Fore.GREEN + text + Style.RESET_ALL
+        case 'DEBUG':
+            return Fore.BLUE + text + Style.RESET_ALL
+        case 'ERROR':
+            return Fore.RED + text + Style.RESET_ALL
+        case 'WARNING':
+            return Fore.YELLOW + text + Style.RESET_ALL
+        case _:
+            return text
+
+
--- script_log_files/main.py
+++ script_log_files/main.py
@@ -1,22 +1,22 @@
 import sys
-from script_log_files.load_logs import load_logs
-from script_log_files.count_logs_by_level import count_logs_by_level
-from script_log_files.filter_logs_by_level import filter_logs_by_level
-from script_log_files.display_log_counts import display_log_counts
+from load_logs import load_logs
+from count_logs_by_level import count_logs_by_level
+from display_log_counts import display_log_counts, coloring
+from filter_logs_by_level import filter_logs_by_level
 
 
 def main(args: list):
-    path = args[2]
+    path = args[1]
     list_of_logs = load_logs(path)
     counted_logs = count_logs_by_level(list_of_logs)
     table = display_log_counts(counted_logs)
     print(table)
 
-    if level := args[3]:
-        result = filter_logs_by_level(list_of_logs, level)
-        print(f'Деталі логів для рівня "{level}":')
+    if len(args) == 3:
+        result = filter_logs_by_level(list_of_logs, args[2].upper())
+        print(f"\nДеталі логів для рівня '{coloring(args[2].upper())}':")
         for log in result:
-            print(f'{log['date'], log['time']} - {log["message"]}')
+            print(f'{log['date']} {log['time']} - {log["message"]}')
 
 
 if __name__ == '__main__':
