79bebfd0f99e7656bc18c3dcf344ad65bb0afb7e
Deminform <deminform@gmail.com>
2024-07-12 11:03:05 +0200

    All methods are accomplished

--- script_log_files/main.py
+++ script_log_files/main.py
@@ -1,24 +1,22 @@
 import sys
-from script_log_files import *
+from script_log_files.load_logs import load_logs
+from script_log_files.count_logs_by_level import count_logs_by_level
+from script_log_files.filter_logs_by_level import filter_logs_by_level
+from script_log_files.display_log_counts import display_log_counts
 
 
-def main(*args: list):
-    path = ''
-    cmd_list = []
+def main(args: list):
+    path = args[2]
+    list_of_logs = load_logs(path)
+    counted_logs = count_logs_by_level(list_of_logs)
+    table = display_log_counts(counted_logs)
+    print(table)
 
-    if len(args) >= 2:
-        path, *commands = args
-        cmd_list = commands.strip().lower()
-
-    match cmd_list[0]:
-        case 'info':
-            ...
-        case 'error':
-            ...
-        case 'debug':
-            ...
-        case 'warning':
-            ...
+    if level := args[3]:
+        result = filter_logs_by_level(list_of_logs, level)
+        print(f'Деталі логів для рівня "{level}":')
+        for log in result:
+            print(f'{log['date'], log['time']} - {log["message"]}')
 
 
 if __name__ == '__main__':
